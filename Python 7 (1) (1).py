9cf744a1895af1a995d9349e274def4ba1f26c18
prasanna1413 <52783397+prasanna1413@users.noreply.github.com>
2019-07-13 14:24:37 +0530

    Add files via upload

---
+++ Python 7 (1) (1).py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Transforming Code into Idioatic Python
+# - Replace traditional index manupulation with Python core looping idiomatic
+
+# In[1]:
+
+
+# Looping over a range of numbers - Traditional Approach
+for i in [0,1,2,3,4,5]:
+    print(i**2,end=" ")
+    
+
+
+# In[2]:
+
+
+# Idiomatic Programming
+for i in range(6):
+    print(i**2,end=" ")
+
+
+# ### Looping over a Collection
+
+# In[3]:
+
+
+colors = ['red','green','yellow','purple']
+for i in range(len(colors)):
+    print(colors[i],end=" ")
+
+
+# In[4]:
+
+
+for color in colors:
+    print(color,end=" ")
+
+
+# ### Looping backwards
+
+# In[5]:
+
+
+colors = ['red','green','yellow','purple']
+for i in range(len(colors)-1,-1,-1):
+    print(colors[i],end=" ")
+
+
+# In[6]:
+
+
+for color in reversed(colors):
+    print(color,end=" ")
+
+
+# ### Looping in Sorted List
+
+# In[9]:
+
+
+colors = ['red','green','yellow','purple']
+for color in sorted(colors):
+    print(color,end=" ")
+
+
+# In[10]:
+
+
+colors = ['red','green','yellow','purple']
+for color in sorted(colors,reverse=True):
+    print(color,end=" ")
+
+
+# ###
+# - if x <= y and y <= z :
+# - if x<=y<=z:
+# - if x == True:
+# - if x:
+
+# ### Pandas
+
+# In[11]:
+
+
+import pandas as pd
+
+
+# In[14]:
+
+
+dt = {'Id':[11,12,13,14,15],
+     'first_name':['A','B','C','D','E'],
+        'company':['aa','bb','cc','dd','ee'],
+        'address':['Hyd','Hyd','Hyd','Hyd','Hyd']}
+mydt = pd.DataFrame(dt)
+
+
+# In[15]:
+
+
+print(mydt)
+
+
+# In[16]:
+
+
+import os
+
+
+# In[24]:
+
+
+os.chdir("pathfile")   #saves to certain location
+
+
+# In[23]:
+
+
+os.mkdir("pathfile")
+
+
+# In[25]:
+
+
+mydt.to_csv('WorkingFile.csv',index = False)
+
+
+# ### Reading the data from CSV file
+
+# In[26]:
+
+
+mydata = pd.read_csv('WorkingFile.csv')
+
+
+# In[35]:
+
+
+pwd
+
+
+# In[28]:
+
+
+print(mydata)
+
+
+# In[33]:
+
+
+mydata1 = pd.read_csv('WorkingFile.csv',skiprows=1,
+                      names=["CustId","Name","Company","Address"]) 
+
+
+# In[34]:
+
+
+print(mydata1)
+
+
+# ### Print Only Two columns
+# ### Print the large value from CustID column --max
+# ### Print the least value from CustID column --min
+# ### Average of CustID column -- sum(li)/len(li)
+
+# In[ ]:
+
+
+
+
